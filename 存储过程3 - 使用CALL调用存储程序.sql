b08425e2a2550e935cb9332f3fbc3198505a423d
hbvir <117999300@qq.com>
2016-08-30 10:32:48 +0800

    使用CALL调用存储程序

---
+++ 存储过程3 - 使用CALL调用存储程序.sql
@@ -0,0 +1,17 @@
+DELIMITER $$   
+   
+DROP PROCEDURE IF EXISTS call_example$$   
+CREATE PROCEDURE call_example(employee_id INT, employee_type VARCHAR(20))   
+     NO SQL   
+BEGIN   
+     DECLARE l_bonus_amount NUMERIC(8,2);   
+   
+     IF employee_type='MANAGER' THEN   
+         CALL calc_manager_bonus(employee_id, l_bonus_amount);   
+     ELSE   
+         CALL calc_minion_bonus(employee_id, l_bonus_amount);   
+     END IF;   
+     CALL grant_bonus(employee_id, l_bonus_amount);   
+END$$   
+
+DELIMITER ; 
